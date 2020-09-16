1edee26359231f20cea57458cca31c78f21430f3
Peijin <pjer1316@icloud.com>
2020-09-16 11:28:13 +0800

    add Run command

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
-docker run --rm -i -p 8998:8998 -u root -t peijin/lofarsun:v0.0.2 /bin/bash -c "jupyter-lab --notebook-dir=~ --ip='*' --port=8998 --no-browser --allow-root"
+docker run --rm -i -p 8998:8998 -u root -t peijin/lofarsun /bin/bash -c "jupyter-lab --notebook-dir=~ --ip='*' --port=8998 --no-browser --allow-root"
 # use [-v /mnt/mnt] to mount volumn, also change the working dir
